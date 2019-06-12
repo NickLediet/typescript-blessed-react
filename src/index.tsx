fa838b6a89f5a04f0f73b3aaafee8015e692176d
Nick Lediet <nick@tagsforhope.com>
2019-06-12 09:48:59 -0400

    Add image test

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react'
 import * as blessed from 'blessed'
 import { render } from 'react-blessed'
-import { Bar } from 'react-blessed-contrib'
+import { Bar, Picture } from 'react-blessed-contrib'
 
 const { Component } = React
 
 
 // Rendering a simple centered box with a bar chart
 class App extends Component<any, any> {
-    
+
     public constructor(props: any) {
         super(props)
 
@@ -19,7 +19,7 @@ class App extends Component<any, any> {
 
     componentDidMount() {
         const intervalId = setInterval(this.timer.bind(this), 1000)
-        
+
     }
 
     timer() {
@@ -37,7 +37,12 @@ class App extends Component<any, any> {
                 width="80%"
                 height="80%"
                 border={{ type: 'line' }}
-                style={{ border: { fg: 'blue' } }}>
+                style={{ border: { fg: 'blue' } }}
+            >
+                <Picture
+                    file={process.cwd() + '/assets/goldfish.png'}
+                    cols={30}
+                />
                 <Bar
                     label="Server Utilization (%)"
                     barWidth={4}
@@ -45,7 +50,7 @@ class App extends Component<any, any> {
                     xOffset={0}
                     maxHeight={Math.max(...this.state.barData)}
                     data={{
-                        titles: this.state.barData.map((d:number,i:number) => `bar${i}`),
+                        titles: this.state.barData.map((d: number, i: number) => `bar${i}`),
                         data: this.state.barData
                     }}
                 />
